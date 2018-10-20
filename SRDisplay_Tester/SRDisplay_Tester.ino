863c042fb9b024c73205b0c496f0f623ea845012
Tijunel <35385306+Tijunel@users.noreply.github.com>
2018-10-20 17:16:59 -0600

    Update SRDisplay_Tester/SRDisplay_Tester.ino

--- SRDisplay_Tester/SRDisplay_Tester.ino
+++ SRDisplay_Tester/SRDisplay_Tester.ino
@@ -1,10 +1,8 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include "Adafruit_GFX.h"
 #include "RA8875.h"
-#include "ctype.h"
 #include <Adafruit_NeoPixel.h>
-#include <Fonts/FreeSerifBold24pt7b.h>
+#include "fonts/minipixel_24.c"
 
 #define PIN 6
 #define DRIVERWANTSBLOCK 1
@@ -37,7 +35,7 @@ int rpm = 0;
 
 
 void setup() {
-    Serial.begin(1000000);
+    Serial.begin(10000000);
     Serial.println("<Arduino is ready>");
     data[0].key = "rpm";
     data[0].value = "6500";
@@ -49,59 +47,32 @@ void setup() {
     tft.begin(RA8875_800x480);
     tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
     //tft.fillScreen(RA8875_BLACK);
-
+    tft.setRotation(2);
     strip.begin();
     strip.show();
     strip.setBrightness(5);
-  
-  
+    tft.setFont(&minipixel_24);
+    
+    tft.setFontScale(12);
     tft.setTextColor(RA8875_WHITE);//background transparent!!!
-    tft.setCursor(CENTER, 0);
-    tft.setFontScale(3);
+    tft.setCursor(CENTER, CENTER);
     tft.println("N");  
+    
+    const tFont* n = NULL;
+    tft.setFont(n);
+    tft.setFontScale(1.6); 
 }
 int counter = 0;
 int flag = 10;
 
-void loop() {
-   if (rpm >= 12500){
+void loop() 
+{
+  if (rpm >= 12500)
     rpm = 0;
-   }else{
+  else
     rpm += 150;
-   }
   displayTester();
 }
-
-
-
-//void printData(){ 
-// for (int i = 0; i < CHANNELS; i++){
-//  if(data[i].key == "rpm"){
-//    //tft.textEnlarge(1);  
-//    //tft.setTextSize(3);
-//    tft.textSetCursor(0, 90);  
-//    tft.textColor(RA8875_WHITE, RA8875_BLACK);
-//    tft.textWrite("RPM");
-//    tft.textSetCursor(350, 90);
-//    tft.textColor(RA8875_WHITE, RA8875_BLACK);
-//    String t_s = String(data[i].value);                                
-//    char buf[t_s.length() + 1];
-//    t_s.toCharArray(buf, t_s.length()+1);
-//    tft.textWrite(buf);    
-//  }else if (data[i].key == "test"){
-//    tft.textEnlarge(1);  
-//    tft.textSetCursor(0, 40);  
-//    tft.textColor(RA8875_WHITE, RA8875_BLACK);
-//    tft.textWrite("TEST");
-//    tft.textSetCursor(350, 40);
-//    tft.textColor(RA8875_WHITE, RA8875_BLACK);
-//    String t_s = String(data[i].value);                                
-//    char buf[t_s.length() + 1];
-//    t_s.toCharArray(buf, t_s.length()+1);
-//    tft.textWrite(buf);
-//  }
-// }
-//}
 
 void displayTester(){
     tft.setFontScale(1); 
@@ -168,24 +139,6 @@ void displayTester(){
     tft.setTextColor(RA8875_WHITE, RA8875_BLACK);
     tft.print("MAP");
 
-    
-//    int scale = 3;
-//  /* Set font size flags */
-//    tft.writeCommand(0x22);
-//    uint8_t temp = tft.readData();
-//    temp &= ~(0xF); // Clears bits 0..3
-//    temp |= scale << 2;
-//    temp |= scale;
-//    tft.writeData(temp);  
-//    delay(2);
-
-
-
-//    tft.textTransparent(RA8875_RED);
-//    tft.textSetCursor(350, 120);  
-//    tft.textColor(RA8875_WHITE, RA8875_BLACK);
-//    tft.textWrite("N");
-
     int percentage = rpm/12500.0*533;
 
     tft.fillRect(133,440, 533, 20, RA8875_BLACK);
@@ -194,30 +147,32 @@ void displayTester(){
     LED(rpm);
 }
 
-void blockIncrementLED() {
-  for (int i = 10; i < 16; i++) {
+void blockIncrementLED() 
+{
+  for (int i = 10; i < 16; i++)
     strip.setPixelColor(i, 0, 0, 255);
-  }
 }
 
-void LED (int rpm) {
+void LED (int rpm) 
+{
   int numberoflights = (int)(rpm * 0.00152);        //0.00152 = 1/657
-  if (numberoflights > strip.numPixels()) {
+  
+  if (numberoflights > strip.numPixels()) 
     numberoflights = strip.numPixels();
-  }
+    
   for (int i = 0; i < strip.numPixels(); i++) {
-    if (i < numberoflights && i < 6) {
+    if (i < numberoflights && i < 6)
       strip.setPixelColor(i, 0, 255, 0);
-    } else if (i < numberoflights && i < 10) {
+    else if (i < numberoflights && i < 10)
       strip.setPixelColor(i, 255, 0, 0);
-    } else if (i < numberoflights && i < 16 && DRIVERWANTSBLOCK) {
+    else if (i < numberoflights && i < 16 && DRIVERWANTSBLOCK) {
       blockIncrementLED();
       break;
-    } else if (i < numberoflights && i < 16) {
+    } 
+    else if (i < numberoflights && i < 16)
       strip.setPixelColor(i, 0, 0, 255);
-    } else {
+    else
       strip.setPixelColor(i, 0);
-    }
   }
   strip.show();
 }
